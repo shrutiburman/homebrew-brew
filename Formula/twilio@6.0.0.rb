ced38d7e5d93b6ab04852169ed44ca03cdf25770
twilio-dx <team_interfaces+github@twilio.com>
2022-10-18 12:42:05 +0000

    Release 6.0.1

--- Formula/twilio@6.0.0.rb
+++ Formula/twilio@6.0.0.rb
@@ -1,11 +1,11 @@
 require "language/node"
 
-class Twilio < Formula
+class TwilioAT600 < Formula
   desc "unleash the power of Twilio from your command prompt"
   homepage "https://github.com/twilio/twilio-cli"
-  url "https://twilio-cli-dev.s3.amazonaws.com/twilio-v6.0.1/twilio-v6.0.1.tar.gz"
-  version "6.0.1"
-  sha256 "61307aed1a8c89c7ca0cd68ebecb0fe3f9cd87302699d3b216596056893fced6"
+  url "https://twilio-cli-dev.s3.amazonaws.com/twilio-v6.0.0/twilio-v6.0.0.tar.gz"
+  version "6.0.0"
+  sha256 "8ca6eed331e4ae622cd7a2b6e24ba35cf8ca8bf4bb6d0a3bc3f1a4674e884fd5"
   depends_on "node"
 
   def install
